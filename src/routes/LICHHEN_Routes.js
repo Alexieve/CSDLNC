a6533c02a250c5f38986ab46cac905cbe5112997
Thinh Vuong <94957063+thinhcu67si@users.noreply.github.com>
2023-12-10 18:37:38 +0700

    Update LICHHEN_Routes.js

--- src/routes/LICHHEN_Routes.js
+++ src/routes/LICHHEN_Routes.js
@@ -1,20 +1,14 @@
 const {Router} = require('express')
-const NV_LichHen_Controllers = require('../controllers/NV_LichHen_Controllers')
+const LICHHEN_Controllers = require('../controllers/LICHHEN_Controllers')
 const { preventLoginAgain, requireAuth } = require('../middleware/authMiddleware')
-
-
 
 const router = Router()
 
-router.get('/Appointmentmanage', requireAuth, NV_LichHen_Controllers.LichHen_get)
-router.get('/Appointmentmanage/init', requireAuth, NV_LichHen_Controllers.LichHen_get_data)
-router.post('/accept-lichhen', requireAuth, NV_LichHen_Controllers.LichHen_post)
-router.post('/deny-lichhen', requireAuth, NV_LichHen_Controllers.LichHen_post2)
 
-router.get('/updateLH', requireAuth, NV_LichHen_Controllers.updateLH_get);
-router.post('/updateLH', requireAuth, NV_LichHen_Controllers.updateLH_post)
-router.get('/updateLH/search/patients', requireAuth, NV_LichHen_Controllers.search_hsbn_get)
-router.get('/updateLH/search/chinhanh', requireAuth, NV_LichHen_Controllers.search_chinhanh_get)
-router.get('/updateLH/search/nhasi/:MACN', requireAuth, NV_LichHen_Controllers.search_nhasi_get)
+router.get('/createLH', requireAuth, LICHHEN_Controllers.createLH_get)
+router.post('/createLH', requireAuth, LICHHEN_Controllers.createLH_post)
+router.get('/createLH/search/patients', requireAuth, LICHHEN_Controllers.search_hsbn_get)
+router.get('/createLH/search/chinhanh', requireAuth, LICHHEN_Controllers.search_chinhanh_get)
+router.get('/createLH/search/nhasi/:MACN', requireAuth, LICHHEN_Controllers.search_nhasi_get)
 
 module.exports = router;
