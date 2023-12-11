04b7379be751f39a8ebbe765335914b9e0f0c3de
Alexive <vuminhquynh23@gmail.com>
2023-12-11 19:11:27 +0700

    Update Diagram and Add Authentication.

--- src/routes/listNS_Routes.js
+++ src/routes/listNS_Routes.js
@@ -1,13 +1,13 @@
 const { Router } = require('express');
 const listNS_Controllers = require('../controllers/listNS_Controllers');
-const { preventLoginAgain, requireAuth } = require('../middleware/authMiddleware');
+const { requirePermission, requireAuth } = require('../middleware/authMiddleware');
 
 const router = Router();
 
-router.get('/listNS', requireAuth, listNS_Controllers.Nhansu_Nhasi_get);
-router.get('/listNS/init', requireAuth,listNS_Controllers.listNS_get_data);
-router.post('/listNS', requireAuth, listNS_Controllers.Nhansu_Nhasi_post);
-router.get('/createNS', requireAuth, listNS_Controllers.createNS_get)
-router.post('/createNS', requireAuth, listNS_Controllers.createNS_post)
+router.get('/listNS', requirePermission(3), listNS_Controllers.Nhansu_Nhasi_get);
+router.get('/listNS/init', requirePermission(3),listNS_Controllers.listNS_get_data);
+router.post('/listNS', requirePermission(3), listNS_Controllers.Nhansu_Nhasi_post);
+router.get('/createNS', requirePermission(3), listNS_Controllers.createNS_get)
+router.post('/createNS', requirePermission(3), listNS_Controllers.createNS_post)
 
 module.exports = router;
