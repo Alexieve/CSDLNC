5b94b4cb12020e1c7394076ad5a71f29185c9140
NguyenKha03 <98521796+foogles22@users.noreply.github.com>
2024-01-06 15:49:27 +0700

    xu ly dong bo

--- src/middleware/database.js
+++ src/middleware/database.js
@@ -2,7 +2,7 @@ const sql = require('mssql')
 const config = {
   user: 'sa',
   password: '123',
-  server: 'ALEX',
+  server: 'NGUYENKHA',
   database: 'CSDLNC',
   options: {
     instanceName: 'SQLEXPRESS',
